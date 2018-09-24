dd9fbf27f65c7be1f771348b34f3e159946016ef
bcapuyan <36900689+bcapuyan@users.noreply.github.com>
2018-09-24 13:18:07 -0700

    Create php_curl.php

---
+++ php_curl.php
@@ -0,0 +1,21 @@
+<?ph
+include ('config.php')
+
+$str ="";
+
+$ch = curl_init ($str);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json, application/xmll','Content-Type: application/json, application/xml'));
+curl_setopt($ch, CURLOPT_USERPWD, "$snowuser:$snowpassword");
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+$output = curl_exec ($ch);
+//read the json file contents
+$data = json_decode($output, true);
+echo $data;
+//GET TOTAL COUNT OF RETURNED RESULTS
+$count = count($data['result']);
+echo "count<br>";
+$i = '0'
+
+
+?>
