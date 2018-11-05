05b40fb26b2698082d9fab8b01509f634e322d44
bcapuyan <36900689+bcapuyan@users.noreply.github.com>
2018-11-05 13:32:20 -0800

    Update php_curl.php

--- php_curl.php
+++ php_curl.php
@@ -1,6 +1,15 @@
-<?ph
-include ('config.php')
+<?php
 
+################################################
+#                                              #
+# This file will initiate a basic curl request #
+#                                              #
+################################################
+
+# the config.php file should contain your User name and Password for Service Now
+include ('config.php')
+  
+# Enter your CURL URL below
 $str ="";
 
 $ch = curl_init ($str);
@@ -9,13 +18,14 @@ curl_setopt($ch, CURLOPT_USERPWD, "$snowuser:$snowpassword");
 curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
 $output = curl_exec ($ch);
-//read the json file contents
+
+#read the json file contents
 $data = json_decode($output, true);
 echo $data;
-//GET TOTAL COUNT OF RETURNED RESULTS
+
+#GET TOTAL COUNT OF RETURNED RESULTS
 $count = count($data['result']);
 echo "count<br>";
 $i = '0'
-
 
 ?>
